5277ac9897dd015b1c3aecda7747b4109a24b4cf
mcdebarros <mauricio.debarros@ucalgary.ca>
2023-12-05 13:38:13 -0700

    Documented

--- src/goph419lab02/linalg_interp.py
+++ src/goph419lab02/linalg_interp.py
@@ -121,83 +121,161 @@ def gauss_iter_solve(a,b,x0=None,tol=1e-8,alg='seidel'):
     # Warn user if maximum iterations reached before convergence
 
     if count >= max_iter:
-        raise RuntimeWarning(f"")
+        raise RuntimeWarning(f"No convergence after {max_iter} iterations"
+                             + ", returning last updated x vector")
+    
+    # Return calculated x vector
+
     return x
 
 def spline_function(xd,yd,order=3):
 
-    #add to dcstring for unique values in xd
+    """Design n order functions for spline interpolation.
 
-    k_sort = np.argsort(xd)
-    xd = np.array([xd[k] for k in k_sort])
-    yd = np.array([yd[k] for k in k_sort])
+    Parameters
+    ----------
+    xd : numpy.ndarray, shape=(n,1)
+        vector of unique x values in ascending order
+    yd : numpy.ndarray, shape=(n,1)
+        vector of y values
+    order : int, 1 or 2 or 3
+        order of interpolating function
+
+    Returns
+    -------
+    s{order} : function
+        Interpolating function
+    """
+
+    # Check that xd is sorted in ascending order and reorder if necessary
+
+    k_sort = np.argsort(xd) # Array of coefficients for sorted xd vector
+    xd = np.array([xd[k] for k in k_sort]) # Sort xd
+    yd = np.array([yd[k] for k in k_sort]) # Sort yd
+
+    # Check that xd and yd have same shape
+
+    if (xd.shape != yd.shape):
+        raise ValueError(f"xd has length {len(xd)}, yd has length {len(yd)}"
+                         + ", should be same")
+    
+    # Check that order is 1 or 2 or 3
 
     if order not in (1,2,3):
         raise ValueError(f"Chosen order of {order} not supported.")
     
-    N = len(xd)
-    a = yd[:-1]
-    dx = np.diff(xd)
-    dy = np.diff(yd)
-    f1 = dy/dx
+    # Initialize first order differences, f1 vector, a vector
+    
+    N = len(xd) # Number of data points
+    a = yd[:-1] # Trimmed yd vector
+    dx = np.diff(xd) # Vector of xd differences
+    dy = np.diff(yd) # Vector of yd differences
+    f1 = dy/dx # First order difference
+
+    # Calculate interpolation function based on order
 
     if order == 1:
 
+        # Map first order differences to b
+
         b = f1
+
+        # Define first order function
+
         def s1(x):
+
+            # Assign spline location based on value in x
 
             k = (0 if x <=  xd[0]
                  else len(a) - 1 if x >= xd[-1]
                 else np.nonzero(xd<x)[0][-1])
             
+            # Return interpolated value at x
+            
             return a[k] + b[k] * (x - xd[k])
         
-        return s1
-    elif order == 2:
+        # Return first order function
         
-        A0 = np.hstack([np.diag(dx[:-1]),np.zeros((N-2,1))])
-        A1 = np.hstack([np.zeros((N-2,1)),np.diag(dx[1:])])
-        A = np.vstack([np.zeros((1,N-1)),(A0+A1)])
-        B = np.zeros((N-1,))
-        B[1:] = np.diff(f1)
-        A[0,:2] = [1,-1]
+        return s1
+    
+    elif order == 2:
+
+        # Design second order coefficient matrix
+        
+        A0 = np.hstack([np.diag(dx[:-1]),np.zeros((N-2,1))]) # Stack 0 vector to left edge of right-side lower portion
+        A1 = np.hstack([np.zeros((N-2,1)),np.diag(dx[1:])]) # Stack 0 vector to right edge of left-side lower portion
+        A = np.vstack([np.zeros((1,N-1)),(A0+A1)]) # Stack 0 row above combined A0 and A1 matrices
+        B = np.zeros((N-1,)) # Initialize b vector
+        B[1:] = np.diff(f1) # Second order differences
+        A[0,:2] = [1,-1] # Add constant entries to first row
+
+        # Solve for c coefficients w/ numpy.linalg (not sufficiently diagonally dominant for Gauss Seidel) and compute b vector
 
         c = np.linalg.solve(A,B)
         b = f1 - c * dx
 
+        # Define second order spline function
+
         def s2(x):
+
+            # Assign spline location based on value in x
 
             k = (0 if x <=  xd[0]
                  else len(a) - 1 if x >= xd[-1]
                 else np.nonzero(xd<x)[0][-1])
             
+            # Return interpolated values at x
+            
             return a[k] + b[k] * (x - xd[k]) + c[k] * (x - xd[k]) ** 2
-        return s2
-    else:
         
-        A = np.zeros((N,N))
-        A[1:-1,:-2] += np.diag(dx[:-1])
-        A[1:-1,1:-1] += np.diag(2 * (dx[:-1] + dx[1:]))
-        A[1:-1,2:] += np.diag(dx[1:])
-        A[0,:3] = [-dx[1],dx[0]+dx[1],-dx[0]]
-        A[-1,-3:] = [-dx[-1],dx[-1]+dx[-2],-dx[-2]]
+        # Return second order function
+
+        return s2
+    
+    else:
+
+        # Design second order coefficient matrix
+        
+        A = np.zeros((N,N)) # Initialize N by N 0 matrix
+        A[1:-1,:-2] += np.diag(dx[:-1]) # Add lower off-diagonal entries, exclude first and last rows
+        A[1:-1,1:-1] += np.diag(2 * (dx[:-1] + dx[1:])) # Add main diagonal entries, exclude first and last rows
+        A[1:-1,2:] += np.diag(dx[1:]) # Add upper off-diagonal entries, exclude first and last rows
+        A[0,:3] = [-dx[1],dx[0]+dx[1],-dx[0]] # Add first row entries
+        A[-1,-3:] = [-dx[-1],dx[-1]+dx[-2],-dx[-2]] # Add last row entries
+
+        # Design B vector
 
         B = np.zeros((N,))
         B[1:-1] = 3 * np.diff(f1)
 
+        # Use gauss_iter_solve() function to compute c coefficients
+
         c = gauss_iter_solve(A,B)
+
+        # Compute d coefficients
 
         d = np.diff(c) / (3 * dx)
 
+        # Third order difference
+
         b = f1 - c[:-1] * dx - d * dx ** 2
 
+        # Define third order spline function
+
         def s3(x):
+
+            # Assign spline location based on value in x
 
             k = (0 if x <=  xd[0]
                  else len(a) - 1 if x >= xd[-1]
                 else np.nonzero(xd<x)[0][-1])
             
+            # Return interpolated values at x
+            
             return a[k] + b[k] * (x - xd[k]) + c[k] * (x - xd[k]) ** 2 + d[k] * (x - xd[k]) ** 3
+        
+        # Return third order function
+
         return s3
 
 
